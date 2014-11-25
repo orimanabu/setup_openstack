42a85729a1e1b124654a7e1eee1458121e4789ca
Manabu Ori <manabu.ori@gmail.com>
2014-11-26 01:47:02 +0900

    update.

--- host/vm_ctl.sh
+++ host/vm_ctl.sh
@@ -8,13 +8,33 @@ op=$1; shift
 vms="juno-controller juno-network1 juno-network2 juno-compute1"
 
 case ${op} in
+clone)
+	for vm in ${vms}; do
+		echo "==> ${vm}"
+		virt-clone -o centos-minimum -n ${vm} --auto-clone
+	done
+	;;
+edit)
+	for vm in ${vms}; do
+		echo "==> ${vm}"
+		guestfish -i --rw -a /var/lib/libvirt/images/${vm}.qcow2
+	done
+	;;
 start)
 	for vm in ${vms}; do
+		echo "==> ${vm}"
 		virsh start ${vm}
+	done
+	;;
+undefine)
+	for vm in ${vms}; do
+		echo "==> ${vm}"
+		virsh undefine --remove-all-storage ${vm}
 	done
 	;;
 backup)
 	for vm in ${vms}; do
+		echo "==> ${vm}"
 		cp /var/lib/libvirt/images/${vm}.qcow2 .
 		cp /etc/libvirt/qemu/${vm}.xml .
 	done
