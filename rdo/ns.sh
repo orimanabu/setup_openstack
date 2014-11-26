d8cefa36f526a969e59a4e7c04ea6d835eb24999
Manabu Ori <manabu.ori@gmail.com>
2014-11-26 14:36:31 +0900

    update.

--- rdo/ns.sh
+++ rdo/ns.sh
@@ -11,6 +11,20 @@ op=$1; shift
 
 case ${op} in
 dhcp|d)
+	network=$1; shift
+	if [ x"${network}" = x"" ]; then
+		echo "$0 dhcp NETWORK"
+		exit 1
+	fi
+	ip netns | grep dhcp | grep $(neutron net-list | awk '/'${network}'/ {print $2}')
+	;;
+dint)
+	network=$1; shift
+	if [ x"${network}" = x"" ]; then
+		echo "$0 dhcp NETWORK"
+		exit 1
+	fi
+	ip netns exec $($0 d ${network}) ip addr show
 	;;
 router|r)
 	router=$1; shift
@@ -19,6 +33,14 @@ router|r)
 		exit 1
 	fi
 	ip netns | grep router | grep $(neutron router-list | awk '/'${router}'/ {print $2}')
+	;;
+rint)
+	router=$1; shift
+	if [ x"${router}" = x"" ]; then
+		echo "$0 router NAME"
+		exit 1
+	fi
+	ip netns exec $($0 r ${router}) ip addr show
 	;;
 *)
 	echo "unknown op: ${op}"
