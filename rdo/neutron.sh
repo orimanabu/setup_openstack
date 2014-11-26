dc9351a20bf8169b0133c5cecb7607648f41f951
Manabu Ori <manabu.ori@gmail.com>
2014-11-26 20:03:42 +0900

    update.

--- rdo/neutron.sh
+++ rdo/neutron.sh
@@ -1,26 +1,67 @@
 #!/bin/bash
 
+function usage {
+	echo "$0 -n NETWORK -s SUBNET -c CIDR -p PUBLIC_NETWORK -r ROUTER -t TENANT -h OP"
+	exit 1
+}
+
 source subr.sh
+
+#OPTIND
+ha=0
+tenant=test
+cidr="192.168.99.0/24"
+public_network=public
+
+while getopts ":c:t:h" o; do
+	case ${o} in
+	h)      # ha
+		ha=1
+		;;
+	c)
+		cidr=${OPTARG}
+		;;
+	n)
+		_network=${OPTARG}
+		;;
+	s)
+		_subnet=${OPTARG}
+		;;
+	p)
+		_public_network=${OPTARG}
+		;;
+	t)
+		tenant=${OPTARG}
+		;;
+	r)
+		_router=${OPTARG}
+		;;
+	*)
+		echo "unknown option: ${o}"
+		usage
+		;;
+	esac
+done
+shift $((OPTIND - 1))
 
 if [ x"$#" != x"1" ]; then
 	echo "$0 op"
 	exit 1
 fi
 op=$1; shift
-tenant=test
-public_network=public
-network=${tenant}_net
-subnet=${tenant}_net_subnet
-router=router_${tenant}
-cidr="192.168.99.0/24"
+
 str=$(echo ${cidr} | sed -e 's|^\([0-9.]*\)\.0/\([0-9]*\)|prefix=\1 prefixlen=\2|')
 eval ${str}
+network=${_network:-${tenant}_net}
+subnet=${_subnet:-${tenant}_net_subnet}
+router=${_router:-router_${tenant}}
 
 pool_start=${prefix}.100
 pool_end=${prefix}.199
 gateway=${prefix}.1
 
 echo "* op: ${op}"
+echo "* ha: ${ha}"
 echo "* tenant: ${tenant}"
 echo "* public_network: ${public_network}"
 echo "* network: ${network}"
@@ -29,14 +70,19 @@ echo "* router: ${router}"
 echo "* cidr: ${cidr}"
 echo "* prefix: ${prefix}"
 echo "* prefixlen: ${prefixlen}"
-echo "* poos_start: ${poos_start}"
+echo "* pool_start: ${pool_start}"
 echo "* pool_end: ${pool_end}"
 echo "* gateway: ${gateway}"
+
+if [ x"${router}" = x"" -o x"${network}" = x"" -o x"${cidr}" = x"" -o x"${prefix}" = x"" -o x"${prefixlen}" = x"" ]; then
+	usage
+fi
+#exit
 
 case ${op} in
 create)
 	source ${gittop}/keystonerc admin
-	do_command neutron router-create ${router} --ha True
+	do_command neutron router-create --tenant-id $(keystone tenant-list | awk '/'${tenant}'/ {print $2}') --ha True ${router}
 	source ${gittop}/keystonerc ${tenant}
 	do_command neutron router-gateway-set $(neutron router-list | awk '/'${router}'/ {print $2}') $(neutron net-list | awk '/'${public_network}'/ {print $2}')
 	do_command neutron net-create ${network}
