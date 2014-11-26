0779724a3051d0e57a707b7f7c9f04783567e860
Manabu Ori <manabu.ori@gmail.com>
2014-11-26 22:12:49 +0900

    update.

---
+++ host/create_volume.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ x"$#" != x"2" ]; then
+	echo "$0 image size[G]"
+	exit 1
+fi
+image=$1; shift
+size=$1; shift
+
+echo "=> create"
+qemu-img create -f qcow2 ${image} ${size}G
+echo "=> info"
+size=$(qemu-img info ${image} | awk '/^virtual size/ {print $4}' | sed -e 's/^(//')
+echo "=> fallocate"
+fallocate -l ${size} ${image}
+echo "=> info"
+qemu-img info ${image}
