36e53bb2fc4b627fe12cfe61211bf0836982c92d
Manabu Ori <manabu.ori@gmail.com>
2014-11-28 22:51:20 +0900

    add misc post-install nits.

--- rdo-juno/after_install.sh
+++ rdo-juno/after_install.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
 source subr.sh
+controller_node="10.0.1.111"
 compute_nodes="10.0.1.131"
+network_nodes="10.0.1.121 10.0.1.122"
+
 for node in ${compute_nodes}; do
 	do_command -r ${node} openstack-config --set /etc/nova/nova.conf libvirt virt_type kvm
 	do_command -r ${node} openstack-config --get /etc/nova/nova.conf libvirt virt_type
 done
 
-ifcfgdir=/etc/sysconfig/network-scripts
-test -f ${ifcfgdir}/ifcfg-eth2 || sed -e 's/eth1/eth2/' -e 's/br-tenant/br-ex/' ${ifcfgdir}/ifcfg-eth1 > ${ifcfgdir}/ifcfg-eth2
+for node in ${network_nodes}; do
+	ifcfgdir=/etc/sysconfig/network-scripts
+	do_command -r ${node} ssh "test -f ${ifcfgdir}/ifcfg-eth2 || sed -e 's/eth1/eth2/' -e 's/br-tenant/br-ex/' ${ifcfgdir}/ifcfg-eth1 > ${ifcfgdir}/ifcfg-eth2"
+done
 
-f=/etc/hosts
-if [ ! -f ${f}.orig ]; then
-	cp -p ${f} ${f}.orig
-	cat nodefile | while read line; do
-		addr=$(echo ${line} | awk '{print $1}')
-		host=$(echo ${line} | awk '{print $2}')
-		fqdn="${host}.mgmt.local"
-		echo "${addr}	${fqdn} ${host}" >> ${f}
-	done
-fi
+for node in ${controller_node} ${network_nodes} ${compute_nodes}; do
+	f=/etc/hosts
+	ssh ${ssh_options} test -f ${f}.orig > /dev/null 2>&1
+	if [ x"$?" != x"0" ]; then
+		do_command -r ${node} cp -p ${f} ${f}.orig
+		cat nodefile | while read line; do
+			addr=$(echo ${line} | awk '{print $1}')
+			host=$(echo ${line} | awk '{print $2}')
+			fqdn="${host}.mgmt.local"
+			do_command -r ${node} "echo '${addr}	${fqdn} ${host}' >> ${f}"
+		done
+	fi
 
-f=/etc/sysconfig/selinux
-if [ ! -f ${f}.orig ]; then
-	#cp -p ${f} ${f}.orig
-	sed -i.orig -e 's/^SELINUX=.*$/SELINUX=permissive/' ${f}
-fi
+	f=/etc/sysconfig/selinux
+	ssh ${ssh_options} test -f ${f}.orig > /dev/null 2>&1
+	if [ x"$?" != x"0" ]; then
+		#cp -p ${f} ${f}.orig
+		do_command -r ${node} "sed -i.orig -e 's/^SELINUX=.*$/SELINUX=permissive/' ${f}"
+	fi
+done
