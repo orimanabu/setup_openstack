29cd67fd6a2541cf9f740da7561bb90953c8f26e
Manabu Ori <manabu.ori@gmail.com>
2014-11-29 13:34:32 +0900

    update.

--- rdo-juno/glance.sh
+++ rdo-juno/glance.sh
@@ -24,3 +24,10 @@ for name in cirros rhel7; do
 	echo "==> ${name}"
 	glance --os-region-name ${region} image-list | grep ${name} || glance --os-region-name ${region} image-create --name ${name} --is-public true --disk-format qcow2 --container-format bare --file $(eval echo '$'${name})
 done
+
+# curl -O -k -L http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img
+# glance image-create --is-public True --disk-format qcow2 --container-format bare --name cirros --file cirros-0.3.3-x86_64-disk.img
+# curl -O -k -L http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
+# glance image-create --is-public True --disk-format qcow2 --container-format bare --name fedora20 --file Fedora-x86_64-20-20140407-sda.qcow2
+# curl -O -k -L http://download.fedoraproject.org/pub/fedora/linux/updates/20/Images/x86_64/Fedora-x86_64-20-20140407-sda.qcow2
+# glance image-create --is-public True --disk-format qcow2 --container-format bare --name ubuntu12.04 --file trusty-server-cloudimg-amd64-disk1.img
