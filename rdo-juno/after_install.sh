e22d822ea3a6aa0e368a6640e06f71941c914658
Manabu Ori <manabu.ori@gmail.com>
2014-11-29 15:11:07 +0900

    update.

--- rdo-juno/after_install.sh
+++ rdo-juno/after_install.sh
@@ -5,16 +5,24 @@ controller_node="10.0.1.111"
 compute_nodes="10.0.1.131"
 network_nodes="10.0.1.121 10.0.1.122"
 
+# only compute nodes
 for node in ${compute_nodes}; do
 	do_command -r ${node} openstack-config --set /etc/nova/nova.conf libvirt virt_type kvm
 	do_command -r ${node} openstack-config --get /etc/nova/nova.conf libvirt virt_type
 done
 
+# only network nodes
 for node in ${network_nodes}; do
 	ifcfgdir=/etc/sysconfig/network-scripts
 	do_command -r ${node} "test -f ${ifcfgdir}/ifcfg-eth2 || sed -e 's/eth1/eth2/' -e 's/br-tenant/br-ex/' ${ifcfgdir}/ifcfg-eth1 > ${ifcfgdir}/ifcfg-eth2"
 done
 
+# controller node and network nodes
+for node in ${controller_node} ${network_nodes}; do
+	do_command -r ${node} openstack-config --set /etc/neutron/neutron.conf DEFAULT l3_ha True
+done
+
+# all nodes
 for node in ${controller_node} ${network_nodes} ${compute_nodes}; do
 	echo "=> /etc/hosts"
 	f=/etc/hosts
