30afd5751d1be66bbddfa5be54b5c39c534707bc
Manabu Ori <manabu.ori@gmail.com>
2014-12-01 12:22:41 +0900

    update.

---
+++ rdo-juno/tcpdump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ x"$#" != x"2" ]; then
+	echo "$0 flavor interface"
+	exit 1
+fi
+
+flavor=$1; shift
+interface=$1; shift
+
+echo tcpdump -w pcap-${flavor}-${interface} -i ${interface}
+tcpdump -w pcap-${flavor}-${interface} -i ${interface}
