1d360c0f4af1f4060153f764fd50cab79eb7069b
Manabu Ori <manabu.ori+github@gmail.com>
2014-12-15 16:54:35 +0900

    update.

---
+++ rhelosp5/cinder_multi_backends.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+test=echo
+
+#backends="lvm_local,lvm_nfs"
+declare -a backends=("lvm_local" "lvm_nfs")
+declare -a vg=("cinder-volumes" "cinder-volumes-nfs")
+declare -a drivers=("cinder.volume.drivers.lvm.LVMISCSIDriver" "cinder.volume.drivers.lvm.LVMISCSIDriver")
+declare -a types=("Local" "NFS")
+declare -a names=("LVM on Local Storage" "LVM on NFS Storage")
+
+default_type=${types[1]}
+
+echo "=> setting /etc/cinder/cinder.conf"
+${test} openstack-config --set /etc/cinder/cinder.conf DEFAULT enable_backends $(echo ${backends[@]} | tr ' ' ',')
+${test} openstack-config --set /etc/cinder/cinder.conf DEFAULT default_volume_type ${default_type}
+i=0
+for backend in ${backends[@]}; do
+	${test} openstack-config --set /etc/cinder/cinder.conf ${backend} volume_group=${vg[$i]}
+	${test} openstack-config --set /etc/cinder/cinder.conf ${backend} volume_driver=${drivers[$i]}
+	${test} openstack-config --set /etc/cinder/cinder.conf ${backend} volume_backend_name=\"${names[$i]}\"
+	let i++
+done
+
+echo "=> restarting cinder related services"
+${test} openstack-service restart cinder
+
+echo "=> creating cinder types"
+i=0
+for type in ${types[@]}; do
+	${test} cinder type-create ${type}
+	${test} cinder type-key ${type} set volume_backend_name=\"${names[$i]}\"
+	let i++
+done
+
+echo "=> showing results"
+${test} cinder type-list
+${test} cinder extra-specs-list
