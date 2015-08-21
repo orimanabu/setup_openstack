8aa691704d28c6e18134323ed298f964c2bddf9a
Manabu Ori <manabu.ori+github@gmail.com>
2015-08-21 19:00:23 +0900

    update.

--- common/prepare.sh
+++ common/prepare.sh
@@ -12,8 +12,17 @@ keyname=sshkey
 echo "* rcfile: ${rcfile}"
 echo "* tenant: ${tenant}"
 
-source ${rcfile}
+source ~/keystonerc_admin
+flavor=m1.nano
+nova flavor-list | grep ${flavor} > /dev/null 2>&1
+if [ x"$?" = x"0" ]; then
+	echo "m1.nano exists"
+else
+	echo "creating m1.nano..."
+	do_command nova flavor-create m1.nano 6 64 1 1
+fi
 
+source ${rcfile}
 nova keypair-list | grep ${keyname} > /dev/null 2>&1
 if [ x"$?" = x"0" ]; then
 	echo "keyname:${sshkey} exists, skip..."
@@ -26,5 +35,7 @@ do_command neutron security-group-rule-create --protocol icmp --direction ingres
 do_command neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress default
 do_command neutron security-group-rule-create --protocol tcp --port-range-min 3389 --port-range-max 3389 --direction ingress default # RDP
 
+do_command nova flavor-list
 do_command nova keypair-list
 #neutron security-group-rule-list | awk '/ default / {print $2}' | while read id; do echo "=> ${id}"; neutron security-group-rule-show ${id}; done
+
