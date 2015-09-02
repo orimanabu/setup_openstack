90926251d43fd38f4116d2c9523d90bcbd39192c
Manabu Ori <manabu.ori+github@gmail.com>
2015-09-03 03:49:46 +0900

    update.

--- common/etcbackup.sh
+++ common/etcbackup.sh
@@ -1,1 +1,10 @@
-tar zcvf ~/controller_etc.tar.gz ceilometer/ cinder/ glance/ keystone/ mongo* my.cnf* neutron/ nova/ openstack-dashboard/ rabbitmq/ redis* sysctl.*
+#!/bin/bash
+
+if [ x"$#" != x"1" ]; then
+	echo "$0 controller|compute"
+	exit 1
+fi
+node=$1; shift
+
+cd /etc
+tar Jcvf ~/etc_${node}.tar.xz ceilometer/ cinder/ glance/ keystone/ mongo* my.cnf* neutron/ nova/ openstack-dashboard/ rabbitmq/ redis* sysctl.*
