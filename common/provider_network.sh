ef9d8a7575c35e544cef36d66a3c0f3f4753126a
Manabu Ori <manabu.ori+github@gmail.com>
2016-01-27 23:15:43 +0900

    update.

--- common/provider_network.sh
+++ common/provider_network.sh
@@ -13,11 +13,15 @@ controller_nodes=10.0.1.111
 network_nodes=10.0.1.121
 compute_nodes="10.0.1.131 10.0.1.132"
 vlanid=10
-network=10.${vlanid}.1
-prefix=24
+netname=provider_net_${vlanid}
+subnetname=provider_subnet_${vlanid}
+routername=provider_router_${vlanid}
+portname=router_port_for_vlan${vlanid}
+prefix=10.1.${vlanid}
+netmask=24
 ns=8.8.8.8
-gateway=${network}.254
-l3agent_addr=${network}.10
+gateway=${prefix}.254
+l3agent_addr=${prefix}.10
 
 case ${op} in
 config)
@@ -49,10 +53,15 @@ config)
 	;;
 create)
 	source ${rcfile}
-	do_command neutron net-create provider_net_${vlanid} --shared --provider:network_type vlan --provider:physical_network physnet-tenant --provider:segmentation_id ${vlanid}
-	do_command neutron subnet-create provider_net_${vlanid} ${network}.0/${prefix} --name provider_subnet_${vlanid} --gateway ${gateway} --dns-nameserver ${ns} --allocation-pool start=${network}.100,end=${network}.199 --host-route destination=169.254.169.254/32,nexthop=${l3agent_addr} --host-route destination=0.0.0.0/0,nexthop=${gateway}
-	do_command neutron router-create provider_router_${vlanid}
-	do_command neutron port-create --fixed-ip subnet_id=$(neutron subnet-list | awk '/'provider_subnet_${vlanid}'/ {print $2}'),ip_address=${l3agent_addr} --name provider_router_port_for_vlan${vlanid} provider_net_${vlanid}
-	do_command neutron router-interface-add provider_router_${vlanid} port=provider_router_port_for_vlan${vlanid}
+	do_command neutron net-create ${netname} --shared --provider:network_type vlan --provider:physical_network physnet-tenant --provider:segmentation_id ${vlanid}
+	do_command neutron subnet-create ${netname} ${prefix}.0/${netmask} --name ${subnetname} --gateway ${gateway} --dns-nameserver ${ns} --allocation-pool start=${prefix}.100,end=${prefix}.199 --host-route destination=169.254.169.254/32,nexthop=${l3agent_addr} --host-route destination=0.0.0.0/0,nexthop=${gateway}
+	do_command neutron router-create ${routername}
+	do_command neutron port-create --fixed-ip subnet_id=$(neutron subnet-list | awk '/'${subnetname}'/ {print $2}'),ip_address=${l3agent_addr} --name ${portname} ${netname}
+	do_command neutron router-interface-add ${routername} port=${portname}
 	;;
+delete)
+	source ${rcfile}
+	do_command neutron router-interface-delete ${routername} port=${portname}
+	do_command neutron router-delete ${routername}
+	do_command neutron net-delete ${netname}
 esac
