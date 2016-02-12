2c3bff238302f1802f48055b195ddbf345708842
Manabu Ori <manabu.ori+github@gmail.com>
2016-02-12 18:40:15 +0900

    refactoring.

--- common/ospcode.sh
+++ common/ospcode.sh
@@ -6,4 +6,9 @@ if [ x"$#" != x"1" ]; then
 fi
 output=$1; shift
 
-cd / && tar --exclude='*.pyc' --exclude='*.pyo' -Jcvf ${output} ./etc/{ahc-tools,ceilometer,cinder,facter,glance,haproxy,heat,httpd,ironic*,iscsi,keepalived,kernel,keystone,libvirt,logrotate*,mongo*,my.cnf*,neutron,nova,openstack-dashboard,openvswitch,os-*,pki,puppet,python,rabbitmq,redis*,rsync*,rsyslog*,snmp,swift,sysconfig,sysctl*,tripleo,tuskar,udev} ./usr/lib/python2.7/site-packages/ ./usr/share/{cinder,glance,haproxy,heat,instack-undercloud,ipxe,ironic,javascript,keystone,libvirt,neutron,nova,novnc,openstack*,openvswitch,packstack,puppet,tripleo*} ./var/lib/{cloud,dnsmasq,haproxy,heat*,hiera,iscsi,NetworkManager,neutron,openstack-dashboard,openvswitch,os-*,puppet,rabbitmq,tripleo}
+etc_files=$(echo ./etc/{ahc-tools,ceilometer,cinder,facter,glance,haproxy,heat,httpd,ironic*,iscsi,keepalived,kernel,keystone,libvirt,logrotate*,mongo*,my.cnf*,neutron,nova,openstack-dashboard,openvswitch,os-*,pki,puppet,python,rabbitmq,redis*,rsync*,rsyslog*,snmp,swift,sysconfig,sysctl*,tripleo,tuskar,udev})
+usrlib_files=$(echo ./usr/lib/python2.7/site-packages)
+usrshare_files=$(echo ./usr/share/{cinder,glance,haproxy,heat,instack-undercloud,ipxe,ironic,javascript,keystone,libvirt,neutron,nova,novnc,openstack*,openvswitch,packstack,puppet,tripleo*})
+varlib_files=$(echo ./var/lib/{cloud,dnsmasq,haproxy,heat*,hiera,iscsi,NetworkManager,neutron,openstack-dashboard,openvswitch,os-*,puppet,rabbitmq,tripleo})
+
+cd / && tar --exclude='*.pyc' --exclude='*.pyo' -Jcvf ${output} ${etc_files} ${usrlib_files} ${usrshare_files} ${varlib_files}
