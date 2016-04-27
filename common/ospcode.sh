7893cbb6bcee56a37f141ebb85cff9988caa0164
Manabu Ori <manabu.ori+github@gmail.com>
2016-04-27 15:27:55 +0900

    style fix.

--- common/ospcode.sh
+++ common/ospcode.sh
@@ -6,9 +6,96 @@ if [ x"$#" != x"1" ]; then
 fi
 output=$1; shift
 
-etc_files=$(echo ./etc/{ahc-tools,ceilometer,cinder,facter,glance,haproxy,heat,httpd,ironic*,iscsi,keepalived,kernel,keystone,libvirt,logrotate*,mongo*,my.cnf*,neutron,nova,openstack-dashboard,openvswitch,os-*,pki,puppet,python,rabbitmq,redis*,rsync*,rsyslog*,snmp,swift,sysconfig,sysctl*,tripleo,tuskar,udev})
+etc_files=$(echo \
+./etc/{ahc-tools,\
+ceilometer,\
+cinder,\
+facter,\
+glance,\
+haproxy,\
+heat,\
+httpd,\
+ironic*,\
+iscsi,\
+keepalived,\
+kernel,\
+keystone,\
+libvirt,\
+logrotate*,\
+mongo*,\
+my.cnf*,\
+neutron,\
+nova,\
+openstack-dashboard,\
+openvswitch,\
+os-*,\
+pki,\
+puppet,\
+python,\
+rabbitmq,\
+redis*,\
+rsync*,\
+rsyslog*,\
+snmp,\
+swift,\
+sysconfig,\
+sysctl*,\
+tripleo,\
+tuskar,\
+udev})
 usrlib_files=$(echo ./usr/lib/python2.7/site-packages)
-usrshare_files=$(echo ./usr/share/{cinder,glance,haproxy,heat,instack-undercloud,ipxe,ironic,javascript,keystone,libvirt,neutron,nova,novnc,openstack*,openvswitch,packstack,puppet,tripleo*})
-varlib_files=$(echo ./var/lib/{cloud,dnsmasq,haproxy,heat*,hiera,iscsi,NetworkManager,neutron,openstack-dashboard,openvswitch,os-*,puppet,rabbitmq,tripleo})
+usrshare_files=$(echo \
+./usr/share/{cinder,\
+glance,\
+haproxy,\
+heat,\
+instack-undercloud,\
+ipxe,\
+ironic,\
+javascript,\
+keystone,\
+libvirt,\
+neutron,\
+nova,\
+novnc,\
+openstack*,\
+openvswitch,\
+packstack,\
+puppet,\
+tripleo*})
+varlib_files=$(echo \
+./var/lib/{cloud,\
+dnsmasq,\
+haproxy,\
+heat*,\
+hiera,\
+iscsi,\
+NetworkManager,\
+neutron,\
+openstack-dashboard,\
+openvswitch,\
+os-*,\
+puppet,\
+rabbitmq,\
+tripleo})
 
-cd / && tar --exclude='*.db' --exclude='*.builder' --exclude='*.ring.gz' --exclude='*.bin' --exclude='*.png' --exclude='*.jpg' --exclude='*.gif' --exclude='*.dat' --exclude='*.eot' --exclude='*.ttf' --exclude='*.woff' --exclude='*.mo' --exclude='*.ico' --exclude='*.odt' --exclude='*.ez' --exclude='*.pyc' --exclude='*.pyo' -Jcvf ${output} ${etc_files} ${usrlib_files} ${usrshare_files} ${varlib_files}
+cd / && tar \
+--exclude='*.db' \
+--exclude='*.builder' \
+--exclude='*.ring.gz' \
+--exclude='*.bin' \
+--exclude='*.png' \
+--exclude='*.jpg' \
+--exclude='*.gif' \
+--exclude='*.dat' \
+--exclude='*.eot' \
+--exclude='*.ttf' \
+--exclude='*.woff' \
+--exclude='*.mo' \
+--exclude='*.ico' \
+--exclude='*.odt' \
+--exclude='*.ez' \
+--exclude='*.pyc' \
+--exclude='*.pyo' \
+-Jcvf ${output} \
+${etc_files} ${usrlib_files} ${usrshare_files} ${varlib_files}
