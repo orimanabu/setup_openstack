d600f92886de4d6a08857a173f53cc7879c1a95c
Manabu Ori <manabu.ori@gmail.com>
2016-07-27 10:43:29 +0900

    rename: rc_append.txt => rc_append_prompt.txt

--- overcloudrc2keystonerc.sh
+++ overcloudrc2keystonerc.sh
@@ -8,4 +8,4 @@ tenant=$1; shift
 
 cat ~/overcloudrc ./rc_append.txt > ~/keystonerc_admin
 cat ~/keystonerc_admin | sed -e "s/admin/${tenant}/g" -e "s/OS_PASSWORD=.*$/OS_PASSWORD=${tenant}/" > ~/keystonerc_${tenant}
-cat rc_append.txt | sed -e 's/(overcloud_.*)/(undercloud)/' >> ~/stackrc
+cat rc_append_prompt.txt | sed -e 's/(overcloud_.*)/(undercloud)/' >> ~/stackrc
