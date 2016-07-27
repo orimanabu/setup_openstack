7551c23872b98cc5768e7e1b60dd1a8742769d4b
Manabu Ori <manabu.ori@gmail.com>
2016-07-27 10:56:07 +0900

    generate Keystone v3 rc files.

--- overcloudrc2keystonerc.sh
+++ overcloudrc2keystonerc.sh
@@ -7,5 +7,7 @@ fi
 tenant=$1; shift
 
 cat ~/overcloudrc ./rc_append_prompt.txt > ~/keystonerc_admin
+cat ~/overcloudrc ./rc_append_v3.txt | sed -e "/OS_AUTH_URL/s/v2\.0$/v3/" > ~/keystonerc_admin_v3
 cat ~/keystonerc_admin | sed -e "s/admin/${tenant}/g" -e "s/OS_PASSWORD=.*$/OS_PASSWORD=${tenant}/" > ~/keystonerc_${tenant}
+cat ~/keystonerc_admin_v3 | sed -e "s/admin/${tenant}/g" -e "s/OS_PASSWORD=.*$/OS_PASSWORD=${tenant}/" > ~/keystonerc_${tenant}_v3
 cat rc_append_prompt.txt | sed -e 's/(overcloud_.*)/(undercloud)/' >> ~/stackrc
