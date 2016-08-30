ab9b43a5002faa2b2e48ce3d38f2f8914aba9c48
Manabu Ori <manabu.ori+github@gmail.com>
2016-08-30 20:28:04 +0900

    refactored.

--- common/command_help.sh
+++ common/command_help.sh
@@ -4,18 +4,12 @@ source ./subr.sh
 outdir=./command_help
 
 mkdir -p ${outdir}
+yum install -y python-tripleoclient
 
-#for p in ${openstack_projects}; do
-#	output=${outdir}/${p}_help.txt
-#	echo "=> ${p} help" 2>&1 | tee ${output}
-#	${p} help 2>&1 | tee -a ${output}
-#	for subcmd in $(sed -e '1,/Positional/d' -e '/^Optional arguments/,$d' ${output} | grep '^    [^ ]' | awk '{print $1}'); do
-#		echo | tee -a ${output}
-#		echo "=> ${p} ${subcmd}" | tee -a ${output}
-#		${p} help ${subcmd} | tee -a ${output}
-#	done
-#done
-for p in ${openstack_projects}; do
+## core projects without neutron and swift
+os_projects_core="nova neutron keystone cinder glance ceilometer heat swift"
+os_projects_core="${os_projects_core} manila sahara ironic trove"
+for p in ${os_projects_core}; do
 	output=${outdir}/${p}_help.md
 	echo "# ${p} help" 2>&1 | tee ${output}
 
@@ -56,14 +50,6 @@ for p in swift; do
 done
 
 ## neutron client
-#for p in neutron; do
-#	output=${outdir}/${p}_help.txt
-#	for subcmd in $(${p} help | sed -e '1,/Commands for API v2.0:/'d -e 's/^  //' -e 's/ .*$//'); do
-#		echo | tee -a ${output}
-#		echo "=> ${p} ${subcmd}" | tee -a ${output}
-#		${p} help ${subcmd} | tee -a ${output}
-#	done
-#done
 for p in neutron; do
 	output=${outdir}/${p}_help.md
 	for subcmd in $(${p} help | sed -e '1,/Commands for API v2.0:/'d -e 's/^  //' -e 's/ .*$//'); do
@@ -78,32 +64,24 @@ for p in neutron; do
 	done
 done
 
-# openstack client
-#output=${outdir}/openstack_help.txt
-#echo "=> openstack --help" 2>&1 | tee ${output}
-#openstack --help 2>&1 | tee -a ${output}
-##for subcmd in $(openstack --help | sed -e '1,/^Commands:/d' -e 's/^  //' -e 's/  .*$//'); do
-#openstack --help | sed -e '1,/^Commands:/d' -e 's/^  //' -e 's/  .*$//' | while read subcmd; do
-#	echo | tee -a ${output}
-#	echo "=> openstack help ${subcmd}" | tee -a ${output}
-#	openstack help ${subcmd} 2>&1 | tee -a ${output}
-#done
-
-output=${outdir}/openstack_help.md
-echo "# openstack --help" 2>&1 | tee ${output}
-echo | tee -a ${output}
-echo '```' 2>&1 | tee -a ${output}
-openstack --help 2>&1 | tee -a ${output}
-echo '```' 2>&1 | tee -a ${output}
-#for subcmd in $(openstack --help | sed -e '1,/^Commands:/d' -e 's/^  //' -e 's/  .*$//'); do
-openstack --help | sed -e '1,/^Commands:/d' -e 's/^  //' -e 's/  .*$//' | while read subcmd; do
-	echo | tee -a ${output}
-	echo | tee -a ${output}
-	echo "# openstack ${subcmd}" | tee -a ${output}
-
+## some clients which has similar output format with openstackclient
+os_projects_osclient_format="openstack aodh gnocchi barbican designate"
+os_projects_osclient_format="openstack"
+for p in ${os_projects_osclient_format}; do
+	output=${outdir}/${p}_help.md
+	echo "# ${p} --help" 2>&1 | tee ${output}
 	echo | tee -a ${output}
 	echo '```' 2>&1 | tee -a ${output}
-	openstack help ${subcmd} 2>&1 | tee -a ${output}
+	${p} --help 2>&1 | tee -a ${output}
 	echo '```' 2>&1 | tee -a ${output}
+	${p} --help | sed -e '1,/^Commands:/d' -e 's/^  //' -e 's/  .*$//' | while read subcmd; do
+		echo | tee -a ${output}
+		echo | tee -a ${output}
+		echo "# ${p} ${subcmd}" | tee -a ${output}
+
+		echo | tee -a ${output}
+		echo '```' 2>&1 | tee -a ${output}
+		${p} help ${subcmd} 2>&1 | tee -a ${output}
+		echo '```' 2>&1 | tee -a ${output}
+	done
 done
-
