60d97cee4c9ac169d621d6ab8cfd9fa98c705270
Manabu Ori <manabu.ori@gmail.com>
2017-03-08 16:52:29 +0900

    cleanup.

--- common/universal_ctags.sh
+++ common/universal_ctags.sh
@@ -6,7 +6,7 @@
 #url=http://tamacom.com/global/${tarball}
 
 echo "=> installing dependent packages..."
-yum install -y gcc make automake autoconf
+sudo yum install -y gcc make automake autoconf
 if [ ! -f ${tarball} ]; then
 	echo "=> ${tarball} not found, fetching..."
 	curl -O "${url}"
@@ -14,13 +14,14 @@ else
 	echo "=> found ${tarball}, use it..."
 fi
 
-if [ -d ctags ]; then
-	echo "=> ctags found, use it..."
+dir=ctags
+if [ -d ${dir} ]; then
+	echo "=> directory '${dir}' found, use it..."
 else
-	echo "=> ctags not found, git cloning..."
+	echo "=> directory '${dir}' not found, git cloning..."
 	git clone https://github.com/universal-ctags/ctags
 	pwd=$(pwd)
-	pushd ./ctags
+	pushd ./${dir}
 #	git remote add masatake-ctags https://github.com/masatake/ctags
 #	git fetch masatake-ctags
 #	git checkout -b masatake-ctags/readtags-qualifier
@@ -31,12 +32,12 @@ if [ -x /usr/local/bin/u-ctags ]; then
 	echo "=> u-ctags found, use it..."
 else
 	echo "=> u-ctags not found, building..."
-	pushd ./ctags
+	pushd ./${dir}
 	./autogen.sh 2>&1 | tee log.autogen
 	./configure --prefix=/usr/local 2>&1 | tee log.configure
 	make 2>&1 | tee log.make
-	make install 2>&1 | tee log.install
-	ln -s /usr/local/bin/ctags /usr/local/bin/u-ctags
+	sudo make install 2>&1 | tee log.install
+	sudo ln -s /usr/local/bin/ctags /usr/local/bin/u-ctags
 	popd
 fi
 
